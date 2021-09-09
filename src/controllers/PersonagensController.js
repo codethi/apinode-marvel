8b086ce764f15d6c95b4540b9d12e97d2dfa942f
codethi <docencia.thiago@gmail.com>
2021-09-08 22:20:14 -0300

    post controller e route

--- src/controllers/PersonagensController.js
+++ src/controllers/PersonagensController.js
@@ -18,13 +18,42 @@ const getById = async (req, res) => {
       res.status(404).json({ message: "Personagem não encontrado" });
       return;
     }
-    return res.send({personagem})
+    return res.send({ personagem });
   } catch (err) {
     res.status(500).send({ error: err });
   }
 };
 
+const create = async (req, res) => {
+  const { nome, identidade, genero, imagem } = req.body;
+
+  if (!nome || !identidade || !genero || !imagem) {
+    res.status(400).send({
+      message: "Você não enviou todos os dados necessários para o cadastro",
+    });
+    return;
+  }
+
+  const novoPersonagem = await new Personagem({
+    nome,
+    identidade,
+    genero,
+    imagem
+  });
+
+  try {
+    await novoPersonagem.save();
+    return res
+      .status(201)
+      .send({ message: "Personagem criado com sucesso", novoPersonagem });
+  } catch (err) {
+      res.status(500).send({error: err})
+  }
+
+};
+
 module.exports = {
   getAll,
   getById,
+  create,
 };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,7 +4,7 @@ const PersonagemController = require("../controllers/PersonagensController");
 const PersonagemMiddleware = require("../middlewares/PersonagemMiddlewares");
 
 routes.get("/personagens", PersonagemController.getAll);
-routes.get("/personagens/:id", PersonagemMiddleware.validaID, PersonagemController.getById)
-
+routes.get("/personagens/:id", PersonagemMiddleware.validaID, PersonagemController.getById);
+routes.post("/personagens", PersonagemController.create);
 
 module.exports = routes
